1d7da001cdd45ce3c39b221ee9fcd92abc0219c5
unknown <devken4u@gmail.com>
2024-07-05 19:19:24 +0800

    import app

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import QrCodeGenerator from "./components/QrCodeGenerator";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <App />
+  <QrCodeGenerator />
   // </React.StrictMode>,
 );
